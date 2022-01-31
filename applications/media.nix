2bee56b0cb2fb646d834ffa35262b2576953af91
Nathan McCarty <nathan@mccarty.io>
2022-01-31 18:04:46 -0500

    Add rclone service

--- applications/media.nix
+++ applications/media.nix
@@ -14,4 +14,16 @@
     # Soulseek client
     nicotine-plus
   ];
+
+  # Mount music directory
+  systemd.user.services.rclone-music = {
+    description = "Rclone mount ~/Music";
+    serviceConfig = {
+      # So we can pick up the fusermount wrapper, this is a less than ideal way to do this
+      Environment = "PATH=/usr/bin:/run/wrappers/bin/";
+      Type = "notify";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount music: /home/nathan/Music --vfs-cache-mode full --vfs-cache-max-size 32Gi --vfs-read-chunk-size 4Mi --vfs-read-ahead 8Mi --config /home/nathan/.config/rclone/rclone.conf --cache-dir /home/nathan/.cache/rclone";
+    };
+    enable = true;
+  };
 }
