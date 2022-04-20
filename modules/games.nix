a3291a86a6f8d02c04f4fc4359a8c9486a78f34c
Nathan McCarty <nathan@mccarty.io>
2022-04-19 20:01:28 -0400

    Patch glfw

--- modules/games.nix
+++ modules/games.nix
@@ -1,11 +1,17 @@
 { pkgs, unstable, ... }: {
-  environment.systemPackages = with unstable; [
-    # Dwarf fortress
-    (dwarf-fortress-packages.dwarf-fortress-full.override {
-      enableFPS = true;
-    })
-    # PolyMC minecraft stuff
-    polymc
-    glfw-wayland
-  ];
+  environment.systemPackages =
+    let
+      glfw-patched = unstable.glfw-wayland.overrideAttrs (attrs: {
+        patches = attrs.patches ++ [ ../patches/minecraft/0003-Don-t-crash-on-calls-to-focus-or-icon.patch ];
+      });
+    in
+    with unstable; [
+      # Dwarf fortress
+      (dwarf-fortress-packages.dwarf-fortress-full.override {
+        enableFPS = true;
+      })
+      # PolyMC minecraft stuff
+      polymc
+      glfw-patched
+    ];
 }
