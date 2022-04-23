fb8a78f90f2a0e0a8441c31feec429846f2443ef
Nathan McCarty <nathan@mccarty.io>
2022-04-22 23:23:55 -0400

    Add fira font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,6 +5,7 @@
     # FiraCode with nerd-fonts patch, as well as fira-code symbols for emacs
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
     fira-code-symbols
+    fira
     # Proportional
     roboto
     liberation_ttf
