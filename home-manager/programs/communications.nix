a9ce73098a8397f156ae3cb198dd73b295a2df33
nathan mccarty <nathan@mccarty.io>
2022-08-18 03:02:08 -0400

    Update discord

--- home-manager/programs/communications.nix
+++ home-manager/programs/communications.nix
@@ -19,10 +19,10 @@
           pname = "discord-electron";
           binaryName = "Discord";
           desktopName = "Discord (Wayland)";
-          version = "0.0.18";
+          version = "0.0.19";
           src = fetchurl {
             url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-            hash = "sha256-BBc4n6Q3xuBE13JS3gz/6EcwdOWW57NLp2saOlwOgMI=";
+            hash = "sha256-GfSyddbGF8WA6JmHo4tUM27cyHV5kRAyrEiZe1jbA5A=";
           };
           electron = pkgs.electron_13;
         };
