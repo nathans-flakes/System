818294bddd69a32275f6dee2b41f3d0e72821cf9
Nathan McCarty <nathan@mccarty.io>
2022-09-04 02:48:19 -0400

    Add nix configuration to droid

--- modules/nix-on-droid/default.nix
+++ modules/nix-on-droid/default.nix
@@ -8,4 +8,21 @@ in
   ];
 
   options = with lib; with nLib; { };
+
+  config = {
+    # Enable unfree packages
+    nixpkgs.config.allowUnfree = config.nathan.config.enableUnfree;
+    # Work around for discord jank ugh
+    nixpkgs.config.permittedInsecurePackages = [
+      "electron-13.6.9"
+    ];
+    # Set system state version
+    system.stateVersion = "22.05";
+    # Enable flakes
+    # Enable nix flakes
+    nix.package = pkgs.nixFlakes;
+    nix.extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 }
