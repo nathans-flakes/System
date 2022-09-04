afe3d9fe2e52a76c618431c8c2bad34f4ee79279
Nathan McCarty <nathan@mccarty.io>
2022-09-04 03:42:31 -0400

    Reeanble binfmt

--- modules/linux/programs/utils.nix
+++ modules/linux/programs/utils.nix
@@ -8,29 +8,8 @@ with lib;
     ../../common/programs/utils.nix
   ];
   config = mkMerge [
-    (mkIf nathan.programs.utils.core
-      {
-        environment.systemPackages = with pkgs; [
-          # Basic command line utilities
-          wget
-          tmux
-          nano
-          unzip
-          any-nix-shell
-          htop
-          # For nslookup
-          dnsutils
-          # Mosh for better high-latency ssh
-          mosh
-          # PV for viewing pipes
-          pv
-        ];
-      })
-    (mkIf nathan.programs.utils.devel {
-      environment.systemPackages = with pkgs; [
-        gcc
-        binutils
-      ];
+    (mkIf nathan.programs.utils.binfmt {
+      boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
     })
   ];
 }
