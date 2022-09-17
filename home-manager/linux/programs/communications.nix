fdbc164adcf193b95bb6cea80ce8272fedc1c22b
Nathan McCarty <nathan@mccarty.io>
2022-09-17 03:35:31 -0400

    Update discord

--- home-manager/linux/programs/communications.nix
+++ home-manager/linux/programs/communications.nix
@@ -19,10 +19,10 @@
           pname = "discord-electron";
           binaryName = "Discord";
           desktopName = "Discord (Wayland)";
-          version = "0.0.19";
+          version = "0.0.20";
           src = fetchurl {
             url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-            hash = "sha256-GfSyddbGF8WA6JmHo4tUM27cyHV5kRAyrEiZe1jbA5A=";
+            hash = "sha256-3f7yuxigEF3e8qhCetCHKBtV4XUHsx/iYiaCCXjspYw=";
           };
           electron = pkgs.electron_13;
         };
